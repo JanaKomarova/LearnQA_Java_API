97ba394179f7e58f7143f7558780d94da6bf8c89
yana.komarova <yana.komarova@rtlabs.ru>
2024-09-06 16:45:44 +0300

    add test CookieTest

---
+++ src/test/java/CookieTest.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CookieTest {
+
+    @Test
+    public void testCookie(){
+        Response responseGetCookie = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        String expectedCookie = responseGetCookie.getCookie("HomeWork");
+        assertEquals("hw_value", expectedCookie, "Unexpected cookie");
+    }
+}
